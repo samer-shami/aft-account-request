6bff0a34c85461d0b57755a0f5944e3052015007
Samer Shami <samer.shami@cmd.com.au>
2022-04-12 16:07:27 +1000

    Add new account for demo purposes

---
+++ terraform/demotime01.tf
@@ -0,0 +1,28 @@
+module "demottime01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws.org.demotime01@example.com"
+    AccountName               = "DemoTime01"
+    ManagedOrganizationalUnit = "DemoTime"
+    SSOUserEmail              = "aws.org.demotime01@example.com"
+    SSOUserFirstName          = "DemoTime"
+    SSOUserLastName           = "01"
+  }
+
+  account_tags = {
+    ManagedBy  = "AFT"
+    CostCenter = "DemoTime01"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "John Smith"
+    change_reason       = "Create a test account for demo purposes"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "demotime01"
+}
